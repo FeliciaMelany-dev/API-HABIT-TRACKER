90b465b76cb9b31adbec39bd8b0ee725e168605f
Felicia Melany <feliciamelany01@gmail.com>
2025-12-03 20:32:18 -0300

    feat(config): configura banco de dados em nuvem

--- src/server.js
+++ src/server.js
@@ -1,17 +1,29 @@
 import app from "./app.js"
 import dotenv from "dotenv";
+import prisma from "./config/prisma.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.status(200).send("BEM VINDO AO HABIT TRACKER!!")
 });
 
+async function iniciandoServer() {
+    try {
 
+        await prisma.$connect();
+        console.log("Conectado ao banco Postgres com sucesso!")
 
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`)});
 
-app.listen(PORT, () =>{
-    console.log(`Servidor rodando na porta ${PORT}`)
-})
+    } catch (error) {
+        console.error("Não foi possível conectar ao banco de dados!")
+        
+        process.exit(1);
+    }
+}
+
+iniciandoServer();
