6b626a3e273231d93901b91f7ef2703115ba0105
Cael <rayssaa.souzaa.26@gmail.com>
2025-12-17 19:29:21 -0300

    fix: reajuste no CORS

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ import registerRoutes from './routes/index.js';
 import { errorHandler } from './middlewares/errorMiddlewares.js'; 
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger.js";
-import cors from "cors"; 
+
 
 const app = express();
 
 ;
 app.use(express.json());
-app.use(cors());
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 registerRoutes(app);
