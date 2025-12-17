945de498c2e255f806ca4c3e0f7f0b86ac93fbb9
Cael <rayssaa.souzaa.26@gmail.com>
2025-12-17 19:23:29 -0300

    fix: Ajuste no cors

--- src/app.js
+++ src/app.js
@@ -7,10 +7,9 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({ origin: "*", methods:["GET","POST","PUT","DELETE"], allowedHeaders:["Content-Type", "Authorization"] }));
-
+;
 app.use(express.json());
-
+app.use(cors());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 registerRoutes(app);
