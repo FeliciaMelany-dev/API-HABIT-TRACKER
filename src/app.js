1acfc344658db28e2a0da464e7a5bdee992da540
Cael <rayssa.souzaa.26@gmail.com>
2025-12-17 22:18:11 -0300

    fix: CORS ajuste

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
+import cors from 'cors';
 import express from 'express';
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger.js";
 import { errorHandler } from './middlewares/errorMiddlewares.js';
 import registerRoutes from './routes/index.js';
-import cors from 'cors';
 
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.options("*", cors());
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
